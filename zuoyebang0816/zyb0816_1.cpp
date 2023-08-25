f06e0522330197eaace19b771bca540640af00c4
tanhx <tanhx99@163.com>
2023-08-25 15:46:06 +0800

    zyb0816

--- zuoyebang0816/zyb0816_1.cpp
+++ zuoyebang0816/zyb0816_1.cpp
@@ -1,106 +1,38 @@
-// 划分链表
-// 给定一个单向链表的头节点head, 节点的值类型是整型,再给定一个整数pivot。实现一个调整链表的函数,将链表调整为左部分
-// 都是值小于pivot的节点,中间部分都是值等于pivot的节点,右部分都是值大于pivot的节点。
-// 要求:
-// 1、在左、中、右三个部分的内部也做顺序要求,要求每部分里的节点从左到右的顺序与原链表中节点的先后次序一致
-// 2、如果链表长度为N,要求时间复杂度O(N),空间复杂度O(1)。
-// 输入描述
-// 9->0->4->5->1, 3
-// 输出描述
-// 调整后是0->1->9->4->5
+//
+// Created by 谭海鑫 on 2023/8/25.
+//
+/*
+ * 最长的斐波那契子序列的长度（力扣873）
+ *
+ * */
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <unordered_map>
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(nullptr) {}
-};
+int lenLongestFibSubseq(vector<int>& arr) {
+    unordered_map<int, int> umap;
+    int n = arr.size();
+    for (int i = 0; i < n; i++) umap[arr[i]] = i;
+    vector<vector<int>> dp(n, vector<int>(n));
+    int ans = 0;
 
-void adjustLinkedList(ListNode *&head, int pivot) {
-    ListNode *lessHead = nullptr, *lessTail = nullptr;
-    ListNode *equalHead = nullptr, *equalTail = nullptr;
-    ListNode *greaterHead = nullptr, *greaterTail = nullptr;
-
-    ListNode *current = head;
-
-    while (current) {
-        ListNode *nextNode = current->next;
-        current->next = nullptr;
-
-        if (current->val < pivot) {
-            if (!lessHead) {
-                lessHead = lessTail = current;
-            } else {
-                lessTail->next = current;
-                lessTail = current;
-            }
-        } else if (current->val == pivot) {
-            if (!equalHead) {
-                equalHead = equalTail = current;
-            } else {
-                equalTail->next = current;
-                equalTail = current;
-            }
-        } else {
-            if (!greaterHead) {
-                greaterHead = greaterTail = current;
-            } else {
-                greaterTail->next = current;
-                greaterTail = current;
-            }
-        }
-
-        current = nextNode;
-    }
-
-    if (lessHead) {
-        if (equalHead) {
-            lessTail->next = equalHead;
-            equalTail->next = greaterHead;
-        } else {
-            lessTail->next = greaterHead;
-        }
-        head = lessHead;
-    } else if (equalHead) {
-        equalTail->next = greaterHead;
-        head = equalHead;
-    } else {
-        head = greaterHead;
-    }
-}
-
-ListNode *createLinkedList(const vector<int> &values) {
-    ListNode *head = nullptr, *current = nullptr;
-    for (int val: values) {
-        ListNode *node = new ListNode(val);
-        if (!head) {
-            head = current = node;
-        } else {
-            current->next = node;
-            current = current->next;
+    for (int i = 0; i < n; i++) {
+        for (int j = i - 1; j >= 0 && arr[j] * 2 > arr[i]; j--) {
+            int k = -1;
+            if (umap.count(arr[i]-arr[j])) k = umap[arr[i]-arr[j]];
+            if (k >= 0) dp[j][i] = max(dp[k][j]+1, 3);
+            ans = max(ans, dp[j][i]);
         }
     }
-    return head;
+    return ans;
 }
 
-void printLinkedList(ListNode *head) {
-    ListNode *current = head;
-    while (current->next) {
-        cout << current->val << "->";
-        current = current->next;
-    }
-    cout << current->val << endl;
-}
 
 int main() {
-    vector<int> values = {9, 0, 4, 5, 1};
-    int pivot = 3;
-
-    ListNode *head = createLinkedList(values);
-    adjustLinkedList(head, pivot);
-    printLinkedList(head);
-
-    return 0;
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) cin >> nums[i];
+    cout << lenLongestFibSubseq(nums) << endl;
 }
--- zuoyebang0816/zyb0816_2.cpp
+++ zuoyebang0816/zyb0816_2.cpp
@@ -1,18 +1,106 @@
-// 约瑟夫环
-
-// 有n个人围成一圈,顺序排号。从第一个人开始报数（从1到3报数),凡报到3的人退出圈子，问最后留下的是原来第几号的那位。
+// 划分链表
+// 给定一个单向链表的头节点head, 节点的值类型是整型,再给定一个整数pivot。实现一个调整链表的函数,将链表调整为左部分
+// 都是值小于pivot的节点,中间部分都是值等于pivot的节点,右部分都是值大于pivot的节点。
+// 要求:
+// 1、在左、中、右三个部分的内部也做顺序要求,要求每部分里的节点从左到右的顺序与原链表中节点的先后次序一致
+// 2、如果链表长度为N,要求时间复杂度O(N),空间复杂度O(1)。
 // 输入描述
-// 8个人为成一圈,顺序编号1,2,3,4,5,6,7,8
+// 9->0->4->5->1, 3
 // 输出描述
-// 最后留下的是原来的第7号
+// 调整后是0->1->9->4->5
+
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-    int n;
-    cin >> n;
-    int pos = 0;
-    for (int i = 2; i <= n; i++) {
-        pos = (pos + 3) % i;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+void adjustLinkedList(ListNode *&head, int pivot) {
+    ListNode *lessHead = nullptr, *lessTail = nullptr;
+    ListNode *equalHead = nullptr, *equalTail = nullptr;
+    ListNode *greaterHead = nullptr, *greaterTail = nullptr;
+
+    ListNode *current = head;
+
+    while (current) {
+        ListNode *nextNode = current->next;
+        current->next = nullptr;
+
+        if (current->val < pivot) {
+            if (!lessHead) {
+                lessHead = lessTail = current;
+            } else {
+                lessTail->next = current;
+                lessTail = current;
+            }
+        } else if (current->val == pivot) {
+            if (!equalHead) {
+                equalHead = equalTail = current;
+            } else {
+                equalTail->next = current;
+                equalTail = current;
+            }
+        } else {
+            if (!greaterHead) {
+                greaterHead = greaterTail = current;
+            } else {
+                greaterTail->next = current;
+                greaterTail = current;
+            }
+        }
+
+        current = nextNode;
     }
-    cout << pos + 1 << endl;
+
+    if (lessHead) {
+        if (equalHead) {
+            lessTail->next = equalHead;
+            equalTail->next = greaterHead;
+        } else {
+            lessTail->next = greaterHead;
+        }
+        head = lessHead;
+    } else if (equalHead) {
+        equalTail->next = greaterHead;
+        head = equalHead;
+    } else {
+        head = greaterHead;
+    }
+}
+
+ListNode *createLinkedList(const vector<int> &values) {
+    ListNode *head = nullptr, *current = nullptr;
+    for (int val: values) {
+        ListNode *node = new ListNode(val);
+        if (!head) {
+            head = current = node;
+        } else {
+            current->next = node;
+            current = current->next;
+        }
+    }
+    return head;
+}
+
+void printLinkedList(ListNode *head) {
+    ListNode *current = head;
+    while (current->next) {
+        cout << current->val << "->";
+        current = current->next;
+    }
+    cout << current->val << endl;
+}
+
+int main() {
+    vector<int> values = {9, 0, 4, 5, 1};
+    int pivot = 3;
+
+    ListNode *head = createLinkedList(values);
+    adjustLinkedList(head, pivot);
+    printLinkedList(head);
+
+    return 0;
 }
