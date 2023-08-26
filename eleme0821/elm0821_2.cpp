f39cf941b6d214f1b57111d3805b9abc5d4b84a9
Cool-tong <tong-1234@qq.com>
2023-08-26 10:14:57 +0800

    format code

--- eleme0821/elm0821_2.cpp
+++ eleme0821/elm0821_2.cpp
@@ -13,26 +13,27 @@ int main() {
     cin >> n >> k;
     int maxCost, minCost;
 
-    if ((n-1)%3 == 0) maxCost = (n-1)/3*2 + k-(n-1);
-    else maxCost = (n-1)/3*2 + 1 + k-(n-1);
+    if ((n - 1) % 3 == 0) maxCost = (n - 1) / 3 * 2 + k - (n - 1);
+    else maxCost = (n - 1) / 3 * 2 + 1 + k - (n - 1);
 
-    if (n%3 == 0) {
+    if (n % 3 == 0) {
         minCost = n / 3;
-        double num = (k-n/3) / (n/3.0);
+        double num = (k - n / 3) / (n / 3.0);
         int m = ceil(num / 2);
-        minCost += m * (n/3);
-    } else if (n%3 == 1) {
-        minCost = (n-1) / 3 + 1;
-        double num = (k - ((n-1)/3+1)) / ((n-1)/3.0);
+        minCost += m * (n / 3);
+    } else if (n % 3 == 1) {
+        minCost = (n - 1) / 3 + 1;
+        double num = (k - ((n - 1) / 3 + 1)) / ((n - 1) / 3.0);
         int m = ceil(num / 2);
-        minCost += m * (n-1)/3;
+        minCost += m * (n - 1) / 3;
     } else {
-        minCost = (n-2) / 3 + 1;
-        double num = (k - ((n-2)/3+1)) / ((n-2)/3.0*2 + 1);
+        minCost = (n - 2) / 3 + 1;
+        double num = (k - ((n - 2) / 3 + 1)) / ((n - 2) / 3.0 * 2 + 1);
         minCost += floor(num);
-        double num2 = (k - ((n-2)/3+1+floor(num))) / ((n-2)/3.0);
+        double num2 = (k - ((n - 2) / 3 + 1 + floor(num))) / ((n - 2) / 3.0);
         int m = ceil(num2 / 2);
-        minCost += m * ((n-2)/3);
+        minCost += m * ((n - 2) / 3);
     }
     cout << maxCost << ' ' << minCost << endl;
 }
+
--- eleme0821/elm0821_3.cpp
+++ eleme0821/elm0821_3.cpp
@@ -17,10 +17,10 @@ int main() {
 
     unordered_map<int, unordered_set<char>> umap;
     for (int i = center, j = center; j >= 0, i < n; i++, j--) {
-        umap[i-center].insert(s1[i]);
-        umap[i-center].insert(s2[i]);
-        umap[center-j].insert(s1[j]);
-        umap[center-j].insert(s2[j]);
+        umap[i - center].insert(s1[i]);
+        umap[i - center].insert(s2[i]);
+        umap[center - j].insert(s1[j]);
+        umap[center - j].insert(s2[j]);
     }
 
     int q;
