d85f70841299cee9ff5775dfaf3c92cb24003716
Cool-tong <tong-1234@qq.com>
2023-09-18 20:26:27 +0800

    fix

--- deeplearning/multihead_attention.py
+++ deeplearning/multihead_attention.py
@@ -21,7 +21,7 @@ class MultiHeadAttention(nn.Module):
         x = x.view(batch_size, -1, self.num_heads, self.d_k)
         return x.transpose(1, 2)
 
-    def forward(self, x, mask=None):
+    def forward(self, x, key_padding_mask=None):
         batch_size = x.size(0)
         Q = self.W_q(x)
         K = self.W_k(x)
@@ -32,9 +32,9 @@ class MultiHeadAttention(nn.Module):
         V = self.split_heads(V, batch_size)
 
         scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.d_k)
-        if mask is not None:
-            mask = mask.unsqueeze(1).unsqueeze(3)
-            scores = scores.masked_fill(mask == 0, -1e9)
+        if key_padding_mask is not None:
+            key_padding_mask = key_padding_mask.unsqueeze(1).unsqueeze(2)
+            scores = scores.masked_fill(key_padding_mask == 0, -1e9)
         attention_weights = F.softmax(scores, dim=-1)
         if self.dropout is not None:
             attention_weights = self.dropout(attention_weights)
@@ -54,10 +54,10 @@ input = torch.rand(batch_size, seq_length, d_model)
 
 # 模拟随机的句子长度
 random_lens = torch.randint(low=1, high=seq_length, size=(batch_size,))
-mask = torch.zeros(batch_size, seq_length)
+key_padding_mask = torch.zeros(batch_size, seq_length)
 for i in range(batch_size):
-    mask[i, :random_lens[i]] = 1
+    key_padding_mask[i, :random_lens[i]] = 1
 
 attention_layer = MultiHeadAttention(d_model, num_heads)
-output = attention_layer(input, mask)
+output = attention_layer(input, key_padding_mask)
 print("Output shape:", output.shape)  # [16, 20, 768]
